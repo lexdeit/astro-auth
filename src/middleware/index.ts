f02bb8163e7e51232698881b9000d33f0d53d660
LEXDEIT <godhood@proton.me>
2024-12-25 23:46:10 -0600

    perf: logic improved/locals values assigned

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,6 @@
 // src/middleware/index.ts
 import { defineMiddleware } from "astro:middleware";
 import { auth } from "@/lib/auth";
-import type { Auth } from "@/lib/auth";
 
 
 const privateRoutes = [
@@ -17,64 +16,17 @@ const publicRoutes = [
 
 export const onRequest = defineMiddleware(async (context, next) => {
 
-    const { url, request } = context;
-
-
+    const { url, request, locals } = context;
+    const { user, session } = await auth.api.getSession({ headers: request.headers }) || {};
+    if (user && session) Object.assign(locals, { user, session });
 
     if (publicRoutes.includes(url.pathname)) {
-
-        const isUserAuth = await isUserAuthenticated({ request, auth });
-
-        if (isUserAuth.isAuthed) {
-            return context.redirect("/protected");
-        }
-
+        return user ? context.redirect("/protected") : next();
     }
 
     if (privateRoutes.includes(url.pathname)) {
-
-        const isUserAuth = await isUserAuthenticated({ request, auth });
-
-        if (isUserAuth.isAuthed) {
-            return next();
-        }
-        else {
-            return context.redirect("/login");
-        }
-
-    } else {
-        return next();
+        return user ? next() : context.redirect("/login");
     }
+
+    return next();
 });
-
-const isUserAuthenticated = async (
-    {
-        request,
-        auth,
-    }: {
-        request: Request;
-        auth: Auth;
-    }) => {
-
-    if (!request.headers) {
-        return {
-            isAuthed: false,
-            message: "No headers found"
-        }
-    }
-
-    const isAuthed = await auth.api.getSession({ headers: request.headers });
-
-    if (!isAuthed) {
-        return {
-            isAuthed: false,
-            message: "User is not authenticated"
-        }
-    } else {
-        return {
-            isAuthed: true,
-            message: "User is authenticated"
-        }
-    }
-
-};
