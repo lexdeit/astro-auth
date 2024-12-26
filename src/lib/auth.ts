a22f85519c4795952eed98fcbd29c20007cb6c7f
LEXDEIT <godhood@proton.me>
2024-12-25 23:22:20 -0600

    feat: added sendemailverification example

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,6 +15,15 @@ export const auth = betterAuth({
     session: {
         expiresIn: 60 * 60 * 24 * 7, // 7 días
         updateAge: 60 * 60 * 24      // 1 día (la expiración se actualiza cada día)
+    },
+    emailVerification: {
+        sendOnSignUp: true,
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+
+            /*AQUI ES DONDE INTEGRARIAMOS LA LOGICA PARA ENVIAR EL CORREO, 
+            LA `URL` QUE RECIBIMOS EN ESTA FUNCION ES LA QUE EL USUARIO TENDRA QUE HACER CLICK PARA VALIDAR SU CORREO*/
+           console.log(url);
+        }
     }
 });
 
